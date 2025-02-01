2774af2810e75f2631dafd9db19e1da47c93cd39
JonatanBadillo <jonatanbadillo.19@gmail.com>
2025-02-01 13:32:20 -0600

    agrega función para aplicar la especificación del histograma utilizando la CDF de referencia

--- tarea-01/Tarea1_JonatanEnriqueBadilloTejeda.py
+++ tarea-01/Tarea1_JonatanEnriqueBadilloTejeda.py
@@ -10,3 +10,17 @@ def compute_cdf(hist):
     cdf_normalized = cdf / float(cdf[-1]) # Normalización dividiendo entre el máximo
     return cdf_normalized
 
+"""Aplica la especificación del histograma a una imagen utilizando la CDF de referencia."""
+def match_histogram(image, ref_cdf):
+    # Calcular el histograma y la CDF de la imagen original
+    hist, bins = np.histogram(image.flatten(), 256, [0,256])
+    cdf_image = compute_cdf(hist)
+    
+    # Crear la función de mapeo
+    mapping = np.interp(cdf_image, ref_cdf, np.arange(256))
+    
+    # Aplicar la transformación
+    corrected_image = np.interp(image.flatten(), np.arange(256), mapping).reshape(image.shape)
+    
+    return corrected_image.astype(np.uint8)
+
